cc69fe0065f714f9e7ac4b396737b2cf9f265f2b
Kabil Ibrahimi <kabili81@hotmail.com>
2022-06-13 18:13:52 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 let myName = "Kabil"
 console.log(myName)
 let salery = "500"
+console.log(salery)
 let contribution = salery * 0.05
+console.log(contribution)
+let taxSalery = (250-80) * 0.04
+console.log(taxSalery)
+let taxSalery1 = (450-250) * 0.08
+console.log(taxSalery1)
+let taxSalery2 = (475-450) * 0.1
+console.log(taxSalery2)
+console.log(salery-contribution-taxSalery-taxSalery1-taxSalery2)
 
+//- 4% on amounts between 80-250 EUR
+//- 8% on amounts between 250-450 EUR
+//- 10% on everything over 450 EUR.
